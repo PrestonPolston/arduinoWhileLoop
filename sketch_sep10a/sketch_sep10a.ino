ad45884179df521a4b0126aa05e57ae281c6fad4
preston <polston13@outlook.com>
2024-09-10 18:36:02 -0400

    started the while loop unable to get the light to blink properly

--- sketch_sep10a/sketch_sep10a.ino
+++ sketch_sep10a/sketch_sep10a.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+int redPin = 9;
+int greenPin = 10;
+int bluePin = 11;
+int count;
+int inputCount;
+int br = 115200;
+int shortPause = 2500;
+String Prompt = "how many times do you want it to flash? ";
 
+
+void setup() {
+Serial.begin(br);
+pinMode(redPin, OUTPUT);
+pinMode(greenPin, OUTPUT);
+pinMode(bluePin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+Serial.println(Prompt);
+while(Serial.available()== 0){}
+inputCount = Serial.parseInt();
+count = 0;
+while(count <= inputCount){
+  analogWrite(redPin, 255);
+  delay(shortPause);
+  analogWrite(redPin, 0);
+  count = count+1;
+  Serial.println(count);
+}
 
 }
